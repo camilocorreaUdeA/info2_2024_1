18eb5b23533080f2742126f6be87c357b24c2285
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2024-02-18 21:43:01 -0500

    Create main.cpp

---
+++ Clase_3/ejemplo_clase/main.cpp
@@ -0,0 +1,19 @@
+/* Ejemplo de compilacion */
+// esto es otro comentario
+
+#include<iostream>
+
+using namespace std;
+
+unsigned int variable = 100;
+
+void mensaje();
+
+int main(){
+    mensaje();
+    return 0;        
+}
+
+void mensaje(){
+    cout<<"Hola"<<endl;
+}
